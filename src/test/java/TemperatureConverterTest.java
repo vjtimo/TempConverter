8bc4af351e05eaea34c56ae95bc4903e1d970841
vjtimo <113420766+vjtimo@users.noreply.github.com>
2024-09-15 21:36:38 +0300

    initial commit

---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+    private TemperatureConverter converter;
+
+    @Test
+    public void testSingleDimensionalArrayEquality() {
+        int[] a1 = {2, 3, 5, 7};
+        int[] a2 = {2, 3, 5, 7};
+        assertArrayEquals("Should be equal", a1, a2);
+    }
+
+    @Test
+    public void testMultiDimensionalArrayEquality() {
+        int[][] a11 = {{2, 3}, {5, 7}, {11, 13}};
+        int[][] a12 = {{2, 3}, {5, 7}, {11, 13}};
+        assertArrayEquals("Should be equal", a11, a12);
+    }
+}
