20e5adfc512818a21869b5aaf90a25b6d31c5c51
vjtimo <113420766+vjtimo@users.noreply.github.com>
2024-09-22 20:22:20 +0300

    Some tests removed

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -8,20 +8,6 @@ public class TemperatureConverterTest {
     private TemperatureConverter converter;
 
     @Test
-    public void testSingleDimensionalArrayEquality() {
-        int[] a1 = {2, 3, 5, 7};
-        int[] a2 = {2, 3, 5, 7};
-        assertArrayEquals("Should be equal", a1, a2);
-    }
-
-    @Test
-    public void testMultiDimensionalArrayEquality() {
-        int[][] a11 = {{2, 3}, {5, 7}, {11, 13}};
-        int[][] a12 = {{2, 3}, {5, 7}, {11, 13}};
-        assertArrayEquals("Should be equal", a11, a12);
-    }
-
-    @Test
     public void testKelvinToCel(){
         assertEquals(0.0, converter.kelvinToCel((float)273.15), 0.01);
     }
